4e385962dc6a334600f1612bba715ba96bfe17ac
Rodion Agutin <rodionagutin@gmail.com>
2026-01-21 22:47:01 +0300

    Init app config and logger

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/rod1kutzyy/url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	logger := setupLogger(cfg.Env)
+	logger.Info("starting url-shortener", slog.String("env", cfg.Env))
+
+	// TODO init storage
+
+	// TODO init router
+
+	// TODO run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
+}
